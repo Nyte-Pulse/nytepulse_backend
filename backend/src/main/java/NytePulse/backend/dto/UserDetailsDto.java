678ea58928f226a4035d851a966519635d0fe813
thilina22 <test@123gmail.com>
2025-10-13 19:43:38 +0530

    Update UserDetailsDto.java

--- backend/src/main/java/NytePulse/backend/dto/UserDetailsDto.java
+++ backend/src/main/java/NytePulse/backend/dto/UserDetailsDto.java
@@ -51,13 +51,13 @@ public class UserDetailsDto {
     public UserDetailsDto() {
     }
 
-    public UserDetailsDto(String userId, String email, String username, String name, String accountType) {
-        this.userId = userId;
-        this.email = email;
-        this.username = username;
-        this.name = name;
-        this.accountType = accountType;
-    }
+//    public UserDetailsDto(String userId, String email, String username, String name, String accountType) {
+//        this.userId = userId;
+//        this.email = email;
+//        this.username = username;
+//        this.name = name;
+//        this.accountType = accountType;
+//    }
 
     // Getters and Setters
     public Long getUserDetailsId() {
