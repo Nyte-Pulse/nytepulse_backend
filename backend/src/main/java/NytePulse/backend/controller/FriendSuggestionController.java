289c2d25ac82df8782eb6aabcb3c736fc5e3c78c
thilina22 <test@123gmail.com>
2025-12-23 23:08:19 +0530

    updated

---
+++ backend/src/main/java/NytePulse/backend/controller/FriendSuggestionController.java
@@ -0,0 +1,35 @@
+package NytePulse.backend.controller;
+
+
+import NytePulse.backend.service.centralServices.FriendSuggestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api/suggestions")
+@CrossOrigin(origins = "*")
+public class FriendSuggestionController {
+
+    @Autowired
+    private FriendSuggestionService friendSuggestionService;
+
+    @GetMapping("/friends")
+    public ResponseEntity<?> getFriendSuggestions(
+            Authentication authentication,
+            @RequestParam(defaultValue = "10") int limit) {
+        String currentUserId = authentication.getName();
+             return friendSuggestionService.getFriendSuggestions(currentUserId, limit);
+    }
+
+    @GetMapping("/mutual-friends/{targetUserId}")
+    public ResponseEntity<?> getMutualFriends(
+            Authentication authentication,
+            @PathVariable String targetUserId) {
+
+        String currentUserId = authentication.getName();
+            return friendSuggestionService.getMutualFriends(currentUserId, targetUserId);
+    }
+}
