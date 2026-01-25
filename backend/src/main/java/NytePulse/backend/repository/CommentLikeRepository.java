876c01d9bb5ffb3f5e48c498cd63de2873a89657
thilina22 <test@123gmail.com>
2026-01-25 23:50:05 +0530

    updated

--- backend/src/main/java/NytePulse/backend/repository/CommentLikeRepository.java
+++ backend/src/main/java/NytePulse/backend/repository/CommentLikeRepository.java
@@ -2,10 +2,14 @@ package NytePulse.backend.repository;
 
 
 import NytePulse.backend.entity.CommentLike;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -20,4 +24,5 @@ public interface CommentLikeRepository extends JpaRepository<CommentLike, Long> 
 
     void deleteByCommentIdAndUserId(Long commentId, Long userId);
 
+    Page<CommentLike> findLikedUsersByCommentId(Long commentId, Pageable pageable);
 }
